348c7ec472ed9fa8a0d6c7f447f410d56e02d362
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2023-05-23 20:46:22 +0800

    Add create notebook route and  method implementation

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -1,10 +1,28 @@
 class NotebooksController < ApplicationController
   include ResponseHelper
-  
+
   def index
     notebooks = Notebook.all
 
     render json: success_res({ notebooks: }, 'All notebooks'),
            status: :ok
+  end
+
+  def create
+    notebook = Notebook.new notebook_params
+
+    if notebook.save
+      render json: success_res({ notebook: }),
+             status: :ok
+    else
+      render json: error_res(notebook.errors.full_messages),
+             status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def notebook_params
+    params.require(:notebook).permit(:name)
   end
 end
--- app/helpers/response_helper.rb
+++ app/helpers/response_helper.rb
@@ -6,4 +6,12 @@ module ResponseHelper
       timestamp: Time.now.to_i
     }
   end
+
+  def error_res(error, message = 'error')
+    {
+      error:,
+      message:,
+      timestamp: Time.now.to_i
+    }
+  end
 end
