a144e30c2c01be167c02ce7187b276321d6ce50d
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2023-05-25 20:14:02 +0800

    Minor changes on layout

--- client/src/components/Notebooks/NBNav/NBNav.tsx
+++ client/src/components/Notebooks/NBNav/NBNav.tsx
@@ -1,11 +1,15 @@
+import { useAuth } from "../../../context/Auth";
 import scss from "./nbnav.module.scss";
 
 const NBNav = () => {
+    const { isAuth } = useAuth();
+
     return (
-        <nav className={scss['nav-container']}>
+        <nav className={scss["nav-container"]}>
             <div className={scss["notebook-nav"]}>Nav</div>
             <div className={scss["actions-container"]}>
                 <button>Add</button>
+                {isAuth && <button>Log out</button>}
             </div>
         </nav>
     );
